edc5199f2949f42e31f57168d1007930e4282e9b
xF1R3ST4Rx <157852178+xF1R3ST4Rx@users.noreply.github.com>
2024-10-24 18:48:59 -0400

    not as buggy with borders

---
+++ Main_project/Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    private Playermovement controls;
+    [SerializeField] private Tilemap ground;
+    [SerializeField] private Tilemap walls;
+    private void Awake()
+    {
+        controls = new Playermovement();
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+    private void Move(Vector2 direction)
+    {
+        if (CanMove(direction))
+        {
+            transform.position += (Vector3)direction;
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = ground.WorldToCell(transform.position + (Vector3)direction);
+        if (!ground.HasTile(gridPosition) || walls.HasTile(gridPosition))
+        {
+            return false;
+        }
+        return true;
+    }
+}
